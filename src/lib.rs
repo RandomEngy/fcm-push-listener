85a9e1c571365781db2c5ed5787c265fd7075f38
David Rickard <davricka@microsoft.com>
2023-02-26 07:45:25 -0800

    Expose more structs in the crate.

--- src/lib.rs
+++ src/lib.rs
@@ -6,5 +6,8 @@ mod listener;
 
 pub use register::register;
 pub use register::Registration;
+pub use register::GcmRegistration;
+pub use fcm::WebPushKeys;
 pub use error::Error;
 pub use listener::FcmPushListener;
+pub use listener::FcmMessage;
